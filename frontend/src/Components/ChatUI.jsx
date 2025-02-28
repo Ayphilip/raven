184b4e40aad5214c947a84077ac2bbaca4dfcc40
Ayphilip <ayolaphilip@gmail.com>
2025-02-28 04:45:50 +0100

    commit

--- frontend/src/Components/ChatUI.jsx
+++ frontend/src/Components/ChatUI.jsx
@@ -231,7 +231,7 @@ function ChatUI({ chats, userDetails, userInfo }) {
 
                         <textarea placeholder="Write your message" value={newMessage} onChange={(e) => setNewMessage(e.target.value)} rows="1" class="w-full resize-none bg-secondery rounded-full px-4 p-2"></textarea>
 
-                        <button type="submit" disabled={newMessage ? false : true} onClick={sendMessage} class="shrink-0 p-2 absolute right-0.5 top-0">
+                        <button type="submit" disabled={newMessage ? false : true} onClick={sendMessageUi} class="shrink-0 p-2 absolute right-0.5 top-0">
                             <ion-icon class="text-xl flex" name="send-outline"></ion-icon>
                         </button>
 
